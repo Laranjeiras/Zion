c16572a5a512d3ecd42976d4f717b100fa60a279
Alexandre Laranjeiras <laranja22@hotmail.com>
2018-07-19 23:57:09 -0300

    Adicionado DateTime.Parts

---
+++ Zion.Common/Extensions/System.DateTime/DateTime.Parts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Zion.Common.Extensions
+{
+    public static partial class ZionExtensions
+    {
+
+        public static DateTime EndOfDay(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, @this.Day).AddDays(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        }
+
+        public static DateTime EndOfMonth(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, 1).AddMonths(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        }
+
+        public static DateTime EndOfYear(this DateTime @this)
+        {
+            return new DateTime(@this.Year, 1, 1).AddYears(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        }
+
+        public static bool IsWeekDay(this DateTime @this)
+        {
+            return !(@this.DayOfWeek == DayOfWeek.Saturday || @this.DayOfWeek == DayOfWeek.Sunday);
+        }
+
+        public static bool IsWeekendDay(this DateTime @this)
+        {
+            return (@this.DayOfWeek == DayOfWeek.Saturday || @this.DayOfWeek == DayOfWeek.Sunday);
+        }
+
+        public static DateTime StartOfDay(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, @this.Day);
+        }
+
+        public static DateTime StartOfMonth(this DateTime @this)
+        {
+            return new DateTime(@this.Year, @this.Month, 1);
+        }
+
+        public static DateTime StartOfYear(this DateTime @this)
+        {
+            return new DateTime(@this.Year, 1, 1);
+        }
+    }
+}
