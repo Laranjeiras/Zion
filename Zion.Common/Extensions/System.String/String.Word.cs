1d3321d4b21f77687717ed378d2b60bbf8755fd6
Alexandre Laranjeiras <laranja22@hotmail.com>
2018-07-19 23:34:00 -0300

    Adicionado String.WordCount

---
+++ Zion.Common/Extensions/System.String/String.Word.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zion.Common.Extensions
+{
+    public partial class ZionExtensions
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },
+                             StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
