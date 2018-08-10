7e65b218c11a7a006791ad71aae3b3d65c0812f2
Alexandre Laranjeiras <laranja22@hotmail.com>
2018-08-10 20:30:53 -0300

    Adicionado Flunt

---
+++ Zion.Common/Flunt/Notifications/Notification.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace Zion.Common.Flunt.Notifications
+{
+    public sealed class Notification
+    {
+        public Notification(string property, string message)
+        {
+            Property = property;
+            Message = message;
+        }
+
+        public string Property { get; private set; }
+        public string Message { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            var notification = obj as Notification;
+            return notification != null &&
+                   Property == notification.Property &&
+                   Message == notification.Message;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1618876500;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Property);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Message);
+            return hashCode;
+        }
+    }
+}
