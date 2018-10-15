380531c6de2e5ca2b3275c126cd0e98721220163
Alexandre Laranjeiras <laranja22@hotmail.com>
2018-10-15 00:09:41 -0300

    Adicionado PlacaVeiculoValidation

---
+++ Zion.Common/Validations/PlacaVeiculoValidation.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Zion.Common.Validations
+{
+    public class PlacaVeiculoValidation
+    {
+        /// <summary>
+        /// Já validando Mercosul formato AAA9A99
+        /// </summary>
+        /// <param name="placaVeiculo"></param>
+        /// <returns></returns>
+        public static bool IsValid(string placaVeiculo)
+        {
+            placaVeiculo = placaVeiculo.Replace("-", string.Empty);
+            if (placaVeiculo.Length != 7) return false;
+
+            Regex rgxPlaca = new Regex(@"^[a-zA-Z]{3}\d{1}\w{1}\d{2}$");
+            return rgxPlaca.IsMatch(placaVeiculo);
+        }
+    }
+}
