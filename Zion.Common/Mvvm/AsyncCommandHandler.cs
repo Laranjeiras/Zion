320b58a3ee5a3c26e693ee1b90b4c703081785f8
Alexandre Laranjeiras <laranja22@hotmail.com>
2018-10-30 19:32:42 -0300

    Adicionado CommandHandlers

---
+++ Zion.Common/Mvvm/AsyncCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Zion.Common.Mvvm
+{
+    public class AsyncCommandHandler : ICommand
+    {
+        private readonly Func<Task> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public AsyncCommandHandler(Func<Task> execute) : this(execute, null)
+        {
+
+        }
+
+        public AsyncCommandHandler(Func<Task> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? (x => true);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? false : _canExecute(parameter);
+        }
+
+        public async void Execute(object parameter)
+        {
+            await _execute();
+        }
+
+        public Task ExecuteAsync(object parameter)
+        {
+            return _execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Zion.Common/Mvvm/CommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Zion.Common.Mvvm
+{
+    public class CommandHandler : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public CommandHandler(Action<object> execute) : this(execute, null)
+        {
+            
+        }
+
+        public CommandHandler(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? (x => true);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? false : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
