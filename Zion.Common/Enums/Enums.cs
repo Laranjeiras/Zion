0c9f763443899b25a0e34e6e35563e2408895db2
Alexandre Laranjeiras <laranja22@hotmail.com>
2018-12-20 12:27:08 -0200

    Adicionado UFs Enums

---
+++ Zion.Common/Enums/Enums.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace Zion.Common.Enums
+{
+    public enum eUF
+    {
+        [Description("Acre")]
+        [XmlEnum("12")]
+        AC = 12,
+        [Description("Alagoas")]
+        [XmlEnum("27")]
+        AL = 27,
+        [Description("Amapá")]
+        [XmlEnum("16")]
+        AP = 16,
+        [Description("Amazonas")]
+        [XmlEnum("13")]
+        AM = 13,
+        [Description("Bahia")]
+        [XmlEnum("29")]
+        BA = 29,
+        [Description("Ceará")]
+        [XmlEnum("23")]
+        CE = 23,
+        [Description("Distrito Federal")]
+        [XmlEnum("53")]
+        DF = 53,
+        [Description("Espírito Santo")]
+        [XmlEnum("32")]
+        ES = 32,
+        [Description("Goiás")]
+        [XmlEnum("52")]
+        GO = 52,
+        [Description("Maranhão")]
+        [XmlEnum("21")]
+        MA = 21,
+        [Description("Mato Grosso")]
+        [XmlEnum("51")]
+        MT = 51,
+        [Description("Mato Grosso do Sul")]
+        [XmlEnum("50")]
+        MS = 50,
+        [Description("Minas Gerais")]
+        [XmlEnum("31")]
+        MG = 31,
+        [Description("Pará")]
+        [XmlEnum("15")]
+        PA = 15,
+        [Description("Paraíba")]
+        [XmlEnum("25")]
+        PB = 25,
+        [Description("Paraná")]
+        [XmlEnum("41")]
+        PR = 41,
+        [Description("Pernambuco")]
+        [XmlEnum("26")]
+        PE = 26,
+        [Description("Piauí")]
+        [XmlEnum("22")]
+        PI = 22,
+        [Description("Rio de Janeiro")]
+        [XmlEnum("33")]
+        RJ = 33,
+        [Description("Rio Grande do Norte")]
+        [XmlEnum("24")]
+        RN = 24,
+        [Description("Rio Grande do Sul")]
+        [XmlEnum("43")]
+        RS = 43,
+        [Description("Rondônia")]
+        [XmlEnum("11")]
+        RO = 11,
+        [Description("Roraima")]
+        [XmlEnum("14")]
+        RR = 14,
+        [Description("Santa Catarina")]
+        [XmlEnum("42")]
+        SC = 42,
+        [Description("São Paulo")]
+        [XmlEnum("35")]
+        SP = 35,
+        [Description("Sergipe")]
+        [XmlEnum("28")]
+        SE = 28,
+        [Description("Tocantins")]
+        [XmlEnum("17")]
+        TO = 17
+    }
+}
