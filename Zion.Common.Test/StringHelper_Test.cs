0af9bc16f9de801435b115c0f5912bbce9375794
Alexandre Laranjeiras <laranja22@hotmail.com>
2018-12-20 12:29:13 -0200

    Adicionado MaxLength StringHelper

---
+++ Zion.Common.Test/StringHelper_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zion.Common.Helpers;
+
+namespace Zion.Common.Test
+{
+    [TestClass]
+    public class StringHelper_Test
+    {
+        [TestMethod]
+        public void MaxLength()
+        {
+            var baseStr = " ABCDEFGHIJKLM ";
+            Assert.AreEqual("ABCDEFGHIJ", StringHelper.MaxLength(baseStr, 10));
+            Assert.AreEqual("", StringHelper.MaxLength(baseStr, 0));
+            Assert.IsNull(StringHelper.MaxLength(null, 10));
+            Assert.AreEqual(baseStr, StringHelper.MaxLength(baseStr, 100, false));
+        }
+    }
+}
--- Zion.Common/Helpers/StringHelper.cs
+++ Zion.Common/Helpers/StringHelper.cs
@@ -19,6 +19,26 @@ namespace Zion.Common.Helpers
             return text;
         }
 
+        /// <summary>
+        /// Return text with maxlength = size
+        /// If size = 0, return string.Empty
+        /// IF text = null, return null
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static string MaxLength(string text, int size, bool allTrim = true)
+        {
+            if (text == null) return null;
+            if (size <= 0) return string.Empty;
+
+            if (allTrim)
+                text = text.TrimEnd().Trim().TrimStart();
+            if (text.Length > size)
+                text = text.Substring(0, size);
+            return text;
+        }
+
         public static bool IsNullOrEmptyOrWhiteSpace(string value)
         {
             return (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value));
