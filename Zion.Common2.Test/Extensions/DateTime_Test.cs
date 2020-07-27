80cf13cce79fb0c6a503403e2edd0f4c433c39d5
Alexandre Laranjeiras <laranja22@hotmail.com>
2020-07-27 19:12:11 -0300

    DateTime.Extesions is not compiling in the file DateTimeExtensions, movo functions to ZionExtensions file

---
+++ Zion.Common2.Test/Extensions/DateTime_Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Zion.Common2.Extensions;
+
+namespace Zion.Common2.Test.Extensions
+{
+    [TestClass]
+    public class DateTime_Test
+    {
+        private readonly DateTime DataBase = new DateTime(2019, 01, 01);
+
+        [TestMethod]
+        public void StartOfDay_Test()
+        {
+            var date = DataBase.StartOfDay();
+            Assert.IsNotNull(date);
+        }
+    }
+}
