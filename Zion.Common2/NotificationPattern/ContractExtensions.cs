4ee5ddb2ba7cffea6357bda07e39f4f56fe97bc9
Alexandre Laranjeiras <laranja22@hotmail.com>
2020-08-16 13:35:35 -0300

    Added ContractExtensions

---
+++ Zion.Common2/NotificationPattern/ContractExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Zion.Common2.NotificationPattern
+{
+    public static class ContractExtension
+    {
+        public static Contract MergeContract(this Contract contractDestino, Contract contractOrigem)
+        {
+            if (contractOrigem == null)
+                throw new ArgumentNullException("MergeContract received null values");
+
+            contractDestino.AddNotifications(contractOrigem.Notifications);
+            return contractDestino;
+        }
+    }
+}
